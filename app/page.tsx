8c167823bd908d5ab2427606ce491c78cb80e969
niera26 <niera26@outlook.com>
2023-08-10 14:50:42 +0200

    reactive margin

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { ButtonList } from "@/components/ButtonList";
 
 export default function Home() {
     return (
-        <main className="container max-w-[618px] mx-auto flex flex-col gap-8 my-20">
+        <main className="container max-w-[618px] mx-auto flex flex-col gap-8 mt-0 mb-20 lg:mt-20">
             <div className="relative w-full h-[220px]">
                 <Image src="/logo.svg" alt="rwalabs" fill priority style={{ objectFit: "contain" }} />
             </div>
             <p className="text-center px-10">
-                At RWA-Labs, we are on a mission to revolutionize the world of crypto by bridging the gap between traditional finance and blockchain technology.
+                At RWA-Labs, we are on a mission to revolutionize the world of crypto by bridging the gap between traditional finance and DeFi.
                 As a cutting-edge crypto company, our primary focus lies in bringing real-world assets onto the blockchain, making them more accessible, secure, and transparent for everyone.
             </p>
             <div className="flex flex-col gap-4">
